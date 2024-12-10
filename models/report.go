e5d3a869de8203ae044e69cf118c54ad14cd0f77
Ignacio Mercado <igmercado46@gmail.com>
2024-12-10 19:17:35 -0300

    feat: adds report model and sqlite driver

---
+++ models/report.go
@@ -0,0 +1,12 @@
+package models
+
+import "gorm.io/gorm"
+
+type Report struct {
+	gorm.Model
+	Category    string  `gorm:"not null"`
+	Title       string  `gorm:"not null"`
+	Description string  `gorm:"not null"`
+	PositionX   float32 `gorm:"not null"`
+	PoitionY    float32 `gorm:"not null"`
+}
