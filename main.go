46631b2188cd099d05105d7b8221ba03dd0117e9
Ignacio Mercado <igmercado46@gmail.com>
2024-12-09 23:07:08 -0300

    feat: sets up .env load

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "8080"
+	}
+
 	r := gin.Default()
 
 	r.GET("/", func(ctx *gin.Context) {
@@ -15,5 +28,5 @@ func main() {
 		})
 	})
 
-	r.Run()
+	r.Run(":" + string(PORT))
 }
