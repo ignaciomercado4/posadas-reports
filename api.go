786f5c00d5d6795111cb939352d6022830f94a3f
Ignacio Mercado <igmercado46@gmail.com>
2024-12-11 03:53:43 -0300

    feat: adds primitive CreateReport func

--- api.go
+++ api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"better-posadas/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,7 +13,25 @@ type ReportHandler struct {
 }
 
 func (h *ReportHandler) CreateReport(c *gin.Context) {
+	var reportInput models.ReportInput
+
+	if err := c.ShouldBind(&reportInput); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "binding error in CreateReport",
+		})
+		return
+	}
+
+	newReport := models.Report{
+		Category:    reportInput.Category,
+		Title:       reportInput.Title,
+		Description: reportInput.Category,
+		PositionX:   reportInput.PositionX,
+		PositionY:   reportInput.PositionY,
+	}
+
+	h.DB.Create(&newReport)
 	c.JSON(http.StatusOK, gin.H{
-		"status": "Create report working",
+		"newReport": newReport,
 	})
 }
