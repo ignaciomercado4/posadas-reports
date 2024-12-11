037158c9160eb0844a100a7320541bb4df97e5a8
Ignacio Mercado <igmercado46@gmail.com>
2024-12-11 04:41:09 -0300

    refactor: changes PosX and PosY data types on report models

--- models/report.go
+++ models/report.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type Report struct {
 	gorm.Model
-	Category    string  `gorm:"not null"`
-	Title       string  `gorm:"not null"`
-	Description string  `gorm:"not null"`
-	PositionX   float32 `gorm:"not null"`
-	PositionY   float32 `gorm:"not null"`
+	Category    string  `json:"category" gorm:"not null"`
+	Title       string  `json:"title" gorm:"not null"`
+	Description string  `json:"description" gorm:"not null"`
+	PositionX   float64 `json:"positionX" gorm:"not null"`
+	PositionY   float64 `json:"positionY" gorm:"not null"`
 }
--- models/reportInput.go
+++ models/reportInput.go
@@ -1,9 +1,9 @@
 package models
 
 type ReportInput struct {
-	Category    string  `gorm:"not null"`
-	Title       string  `gorm:"not null"`
-	Description string  `gorm:"not null"`
-	PositionX   float32 `gorm:"not null"`
-	PositionY   float32 `gorm:"not null"`
+	Category    string  `json:"category" binding:"required"`
+	Title       string  `json:"title" binding:"required"`
+	Description string  `json:"description" binding:"required"`
+	PositionX   float64 `json:"positionX" binding:"required"`
+	PositionY   float64 `json:"positionY" binding:"required"`
 }
